ad0a802979aa7e449f9b4a2b849a28896e50456c
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2024-04-23 22:16:25 +0300

    Automated migration

---
+++ FlowReader.DataAccess/Persistence/AutomatedMigration.cs
@@ -0,0 +1,21 @@
+﻿using FlowReader.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowReader.DataAccess.Persistence
+{
+    public static class AutomatedMigration
+    {
+        public static async Task MigrateAsync(IServiceProvider services)
+        {
+            var context = services.GetRequiredService<DatabaseContext>();
+
+            if (context.Database.IsSqlServer()) await context.Database.MigrateAsync();
+
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await DataSeeder.SeedDatabaseAsync(context, userManager);
+        }
+    }
+}
---
+++ FlowReader.DataAccess/Persistence/DataSeeder.cs
@@ -0,0 +1,20 @@
+﻿using FlowReader.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace FlowReader.DataAccess.Persistence
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedDatabaseAsync(DatabaseContext context, UserManager<ApplicationUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new ApplicationUser { UserName = "admin", Email = "admin@admin.com", EmailConfirmed = true };
+
+                await userManager.CreateAsync(user, "Admin123.?");
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
