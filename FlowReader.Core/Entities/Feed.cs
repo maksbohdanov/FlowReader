43f517f20d99f17593379d62a6ce367eebc51b76
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2024-05-15 16:46:46 +0300

    Initial migration

--- FlowReader.Core/Entities/Feed.cs
+++ FlowReader.Core/Entities/Feed.cs
@@ -5,13 +5,14 @@ namespace FlowReader.Core.Entities
 {
     public class Feed : BaseEntity
     {
+        public string UserTitle { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Link { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime PublishingDate { get; set; }
+        public DateTime? PublishingDate { get; set; }
 
         public virtual ApplicationUser? User { get; set; }
-        public Guid? UserId { get; set; }
+        public string? UserId { get; set; }
         public virtual ICollection<News> News { get; set; } = new HashSet<News>();
         public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
--- FlowReader.DataAccess/Configurations/CategoryConfiguration.cs
+++ FlowReader.DataAccess/Configurations/CategoryConfiguration.cs
@@ -27,8 +27,8 @@ namespace FlowReader.DataAccess.Configurations
                 .WithMany(x => x.Categories)
                 .UsingEntity(
                     "UserFavorite",
-                    l => l.HasOne(typeof(Feed)).WithMany().HasForeignKey("FeedId"),
-                    r => r.HasOne(typeof(ApplicationUser)).WithMany().HasForeignKey("UserId"),
+                    l => l.HasOne(typeof(ApplicationUser)).WithMany().HasForeignKey("UserId"),
+                    r => r.HasOne(typeof(Category)).WithMany().HasForeignKey("CategoryId"),
                     j =>
                     {
                         j.IndexerProperty<Guid>("Id");
