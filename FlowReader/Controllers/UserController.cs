216a372084896999f697892abd2788a323d02a91
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2024-05-26 11:09:56 +0300

    Add user management

---
+++ FlowReader/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using FlowReader.Core.Identity;
+using FlowReader.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowReader.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var users = await _userManager.GetUsersInRoleAsync("User");
+            var model = users.Select(x => new UserViewModel
+            {
+                Id = x.Id,
+                Email = x.Email,
+                UserName = x.UserName
+            });
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var model = new UserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            user.Email = model.Email;
+            user.UserName = model.UserName;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            var result = await _userManager.DeleteAsync(user);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ FlowReader/Models/User/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowReader.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string UserName { get; set; }
+    }
+}
